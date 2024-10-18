1e4b405c61cb32d64a23485f2d16f29a2ce9044a
motezbelhadj <edumotez@gmail.com>
2024-10-18 09:27:23 +0100

    Add files via upload

---
+++ Salaire.java
@@ -0,0 +1,51 @@
+package employe;
+
+public class Salaire {
+	protected int matricule;
+
+ protected String nom;
+ protected int recrutement;
+public Salaire(int matricule, String nom, int recrutement) {
+	this.matricule = matricule;
+	this.nom = nom;
+	this.recrutement = recrutement;
+}
+public Salaire() {
+
+}
+public int getMatricule() {
+	return matricule;
+}
+public void setMatricule(int matricule) {
+	this.matricule = matricule;
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public int getRecrutement() {
+	return recrutement;
+}
+public void setRecrutement(int recrutement) {
+	this.recrutement = recrutement;
+}
+@Override
+public String toString() {
+	return "Salaire [matricule=" + matricule + ", nom=" + nom + ", recrutement=" + recrutement + "]";
+}
+
+ public int salaireDate() {
+	 if(this.recrutement<2005) {
+		 return 400;
+	 }
+	 return 280;
+	 
+	 
+	 
+ }
+ 
+ 
+ 
+}
